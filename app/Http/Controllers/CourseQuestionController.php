fa3a283d1992c8e30a68c93501957efece703b8c
christ-saragih <bennefit.19@gmail.com>
2024-04-15 17:06:51 +0700

    CREATED: Fitur menambahkan questions

--- app/Http/Controllers/CourseQuestionController.php
+++ app/Http/Controllers/CourseQuestionController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use App\Models\CourseQuestion;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 
 class CourseQuestionController extends Controller
 {
@@ -18,17 +21,60 @@ class CourseQuestionController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Course $course)
     {
-        //
+        $students = $course->students()->orderBy('id', 'DESC')->get();
+        return view('admin.questions.create', [
+            'course' => $course,
+            'students' => $students,
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Course $course)
     {
-        //
+        $validated = $request->validate([
+            'question' => 'required|string|max:255',
+            'answers' => 'required|array',
+            'answers.*' => 'required|string',
+            'correct_answer' => 'required|integer'
+        ]);
+
+        DB::beginTransaction();
+
+        try {
+            // membuat sebuah pertanyaan
+            // karena menggunakan relationship, jadi tidak perlu mendeklarasikan id-nya apa dan course_id-nya apa
+            $question = $course->questions()->create([
+                'question' => $request->question,
+            ]);
+
+            // membuat data jawaban
+            foreach($request->answers as $index => $answerText) {
+                // mencari jawaban yang benar
+                $isCorrect = ($request->correct_answer == $index);
+                // menyimpan jawaban
+                $question->answers()->create([
+                    'answer' => $answerText,
+                    'is_correct' => $isCorrect,
+                ]);
+            }
+
+            DB::commit();
+
+            return redirect()->route('dashboard.courses.show', $course->id);
+
+        } catch(\Exception $e) {
+            DB::rollBack();
+            $error = ValidationException::withMessages([
+                'system_error' => ['System error!' . $e->getMessage()],
+            ]);
+
+            throw $error;
+        }
+
     }
 
     /**
